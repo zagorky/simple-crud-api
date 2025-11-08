51d9aad9f02b32bb3fa1f8ce6c9bda76d53386f5
zagorky <zagorkiynahyi@gmail.com>
2025-11-08 16:26:09 +0300

    feat: add Jest configuration for TypeScript testing

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js'],
+  collectCoverageFrom: ['src/**/*.ts'],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+};
