c255a17f861a56c038c90be4a798e6353defbe21
zagorky <zagorkiynahyi@gmail.com>
2025-11-08 14:56:19 +0300

    chore: add type guards for user validation and UUID checks

---
+++ src/utils/type-guards.ts
@@ -0,0 +1,39 @@
+import {User} from '../db';
+
+const UUID_REGEX =
+  /^(?:[0-9a-f]{8}-[0-9a-f]{4}-[1-8][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}|00000000-0000-0000-0000-000000000000|ffffffff-ffff-ffff-ffff-ffffffffffff)$/i;
+
+export const hasProperty = <K extends string>(
+  property: K,
+  source: unknown,
+): source is {
+  [key in K]: unknown;
+} => {
+  return typeof source === 'object' && source !== null && property in source;
+};
+
+export const isString = (data: unknown): data is string => {
+  return typeof data === 'string';
+};
+
+export const isObject = (data: unknown): data is object => {
+  return typeof data === 'object' && data !== null;
+};
+
+export const isArray = <T>(data: unknown, itemGuard?: (item: unknown) => item is T): data is T[] => {
+  return Array.isArray(data) && (itemGuard ? data.every(itemGuard) : true);
+};
+
+export const isValidUserBody = (body: unknown): body is Omit<User, 'id'> =>
+  isObject(body) &&
+  hasProperty('username', body) &&
+  isString(body.username) &&
+  hasProperty('age', body) &&
+  isString(body.age) &&
+  hasProperty('hobbies', body) &&
+  isArray(body.hobbies) &&
+  body.hobbies.every((h: unknown) => typeof h === 'string');
+
+export const isValidUUID = (id: string) => {
+  return UUID_REGEX.test(id);
+};
