d0088972a6f00ce1178a4081193a54b8cbd5bad6
zagorky <zagorkiynahyi@gmail.com>
2025-11-09 20:03:41 +0300

    feat: refactor request handler in server.ts for improved readability and maintainability

--- src/server.ts
+++ src/server.ts
@@ -12,127 +12,129 @@ const setHeaders = (res: ServerResponse) => {
   res.setHeader(HEADERS.ACCESS_CONTROL_ALLOW_HEADERS, HEADERS.CONTENT_TYPE);
 };
 
-export const createAppServer = () => {
-  return createServer(async (req: IncomingMessage, res: ServerResponse) => {
-    setHeaders(res);
+export const addHandler = async (req: IncomingMessage, res: ServerResponse) => {
+  setHeaders(res);
 
-    const url = req.url || '';
-    const method = req.method || '';
+  const url = req.url || '';
+  const method = req.method || '';
 
-    if (method === METHODS.OPTIONS) {
-      logger.request(method, url, STATUS_CODES.NO_CONTENT);
-      res.writeHead(STATUS_CODES.NO_CONTENT);
-      return res.end();
+  if (method === METHODS.OPTIONS) {
+    logger.request(method, url, STATUS_CODES.NO_CONTENT);
+    res.writeHead(STATUS_CODES.NO_CONTENT);
+    return res.end();
+  }
+
+  if (!url.startsWith(ROUTES.USERS)) {
+    logger.error(ERROR_MESSAGES.NOT_FOUND, url);
+    return sendError(res, STATUS_CODES.NOT_FOUND, ERROR_MESSAGES.NOT_FOUND);
+  }
+
+  const idMatch = url.match(ROUTE_REGEX);
+  const id = idMatch ? idMatch[1] : null;
+
+  if (id && !isValidUUID(id)) {
+    logger.error(ERROR_MESSAGES.INVALID_USER_ID, url);
+    return sendError(res, STATUS_CODES.BAD_REQUEST, ERROR_MESSAGES.INVALID_USER_ID);
+  }
+
+  let body;
+  try {
+    body = await parseBody(req);
+  } catch {
+    logger.error(ERROR_MESSAGES.INVALID_JSON, url);
+    return sendError(res, STATUS_CODES.BAD_REQUEST, ERROR_MESSAGES.INVALID_JSON);
+  }
+
+  if (url === ROUTES.USERS && method === METHODS.GET) {
+    const result = await executeDbCommand({ type: 'GET_ALL' });
+    if (!result.success) {
+      return sendError(res, STATUS_CODES.INTERNAL_SERVER_ERROR, result.error);
     }
 
-    if (!url.startsWith(ROUTES.USERS)) {
-      logger.error(ERROR_MESSAGES.NOT_FOUND, url);
-      return sendError(res, STATUS_CODES.NOT_FOUND, ERROR_MESSAGES.NOT_FOUND);
+    logger.request(method, url, STATUS_CODES.OK);
+    logger.info(`Fetching all users.`);
+    res.writeHead(STATUS_CODES.OK);
+    return res.end(JSON.stringify(result.data));
+  }
+
+  if (url === ROUTES.USERS && method === METHODS.POST) {
+    if (!isValidUserBody(body)) {
+      logger.error(ERROR_MESSAGES.INVALID_USER_BODY, `Body: ${JSON.stringify(body)}`);
+      return sendError(res, STATUS_CODES.BAD_REQUEST, ERROR_MESSAGES.INVALID_USER_BODY);
+    }
+    const result = await executeDbCommand({ type: 'CREATE', data: body });
+    if (!result.success) {
+      logger.error(ERROR_MESSAGES.INTERNAL_SERVER_ERROR, `Error: ${result.error}`);
+      return sendError(res, STATUS_CODES.INTERNAL_SERVER_ERROR, result.error);
     }
 
-    const idMatch = url.match(ROUTE_REGEX);
-    const id = idMatch ? idMatch[1] : null;
+    logger.request(method, url, STATUS_CODES.CREATED);
+    logger.info(`User created successfully. ${JSON.stringify(result.data)}`);
+    res.writeHead(STATUS_CODES.CREATED);
+    return res.end(JSON.stringify(result.data));
+  }
 
-    if (id && !isValidUUID(id)) {
-      logger.error(ERROR_MESSAGES.INVALID_USER_ID, url);
-      return sendError(res, STATUS_CODES.BAD_REQUEST, ERROR_MESSAGES.INVALID_USER_ID);
-    }
-
-    let body;
-    try {
-      body = await parseBody(req);
-    } catch {
-      logger.error(ERROR_MESSAGES.INVALID_JSON, url);
-      return sendError(res, STATUS_CODES.BAD_REQUEST, ERROR_MESSAGES.INVALID_JSON);
-    }
-
-    if (url === ROUTES.USERS && method === METHODS.GET) {
-      logger.request(method, url, STATUS_CODES.OK);
-      const result = await executeDbCommand({ type: 'GET_ALL' });
-      if (!result.success) {
-        return sendError(res, STATUS_CODES.INTERNAL_SERVER_ERROR, result.error);
-      }
-      logger.info(`Fetching all users.`);
-      res.writeHead(STATUS_CODES.OK);
-      return res.end(JSON.stringify(result.data));
-    }
-
-    if (url === ROUTES.USERS && method === METHODS.POST) {
-      logger.request(method, url, STATUS_CODES.CREATED);
-      if (!isValidUserBody(body)) {
-        logger.error(ERROR_MESSAGES.INVALID_USER_BODY, `Body: ${JSON.stringify(body)}`);
-        return sendError(res, STATUS_CODES.BAD_REQUEST, ERROR_MESSAGES.INVALID_USER_BODY);
-      }
-      const result = await executeDbCommand({ type: 'CREATE', data: body });
+  if (id) {
+    if (method === METHODS.GET) {
+      const result = await executeDbCommand({ type: 'GET_BY_ID', id });
       if (!result.success) {
         logger.error(ERROR_MESSAGES.INTERNAL_SERVER_ERROR, `Error: ${result.error}`);
         return sendError(res, STATUS_CODES.INTERNAL_SERVER_ERROR, result.error);
       }
-      logger.info(`User created successfully. ${JSON.stringify(result.data)}`);
-      res.writeHead(STATUS_CODES.CREATED);
+      if (!result.data) {
+        logger.error(ERROR_MESSAGES.USER_NOT_FOUND, `User ID: ${id}`);
+        return sendError(res, STATUS_CODES.NOT_FOUND, ERROR_MESSAGES.USER_NOT_FOUND);
+      }
+      logger.request(method, url, STATUS_CODES.OK);
+      logger.info(`User retrieved. ID: ${id}`);
+      res.writeHead(STATUS_CODES.OK);
       return res.end(JSON.stringify(result.data));
     }
 
-    if (id) {
-      if (method === METHODS.GET) {
-        logger.request(method, url, STATUS_CODES.OK);
-        const result = await executeDbCommand({ type: 'GET_BY_ID', id });
-        if (!result.success) {
-          logger.error(ERROR_MESSAGES.INTERNAL_SERVER_ERROR, `Error: ${result.error}`);
-          return sendError(res, STATUS_CODES.INTERNAL_SERVER_ERROR, result.error);
-        }
-        if (!result.data) {
-          logger.error(ERROR_MESSAGES.USER_NOT_FOUND, `User ID: ${id}`);
-          return sendError(res, STATUS_CODES.NOT_FOUND, ERROR_MESSAGES.USER_NOT_FOUND);
-        }
-        logger.info(`User retrieved. ID: ${id}`);
-        res.writeHead(STATUS_CODES.OK);
-        return res.end(JSON.stringify(result.data));
+    if (method === METHODS.PUT) {
+      if (!isValidUserBody(body)) {
+        logger.error(ERROR_MESSAGES.INVALID_USER_BODY, `User ID: ${id}, Body: ${JSON.stringify(body)}`);
+        return sendError(res, STATUS_CODES.BAD_REQUEST, ERROR_MESSAGES.INVALID_USER_BODY);
+      }
+      const result = await executeDbCommand({ type: 'UPDATE', id, data: body });
+
+      if (!result.success) {
+        logger.error(ERROR_MESSAGES.INTERNAL_SERVER_ERROR, `Error: ${result.error}`);
+        return sendError(res, STATUS_CODES.INTERNAL_SERVER_ERROR, result.error);
       }
 
-      if (method === METHODS.PUT) {
-        logger.request(method, url, STATUS_CODES.OK);
-        if (!isValidUserBody(body)) {
-          logger.error(ERROR_MESSAGES.INVALID_USER_BODY, `User ID: ${id}, Body: ${JSON.stringify(body)}`);
-          return sendError(res, STATUS_CODES.BAD_REQUEST, ERROR_MESSAGES.INVALID_USER_BODY);
-        }
-        const result = await executeDbCommand({ type: 'UPDATE', id, data: body });
-
-        if (!result.success) {
-          logger.error(ERROR_MESSAGES.INTERNAL_SERVER_ERROR, `Error: ${result.error}`);
-          return sendError(res, STATUS_CODES.INTERNAL_SERVER_ERROR, result.error);
-        }
-
-        if (!result.data) {
-          logger.error(ERROR_MESSAGES.USER_NOT_FOUND, `User ID: ${id}`);
-          return sendError(res, STATUS_CODES.NOT_FOUND, ERROR_MESSAGES.USER_NOT_FOUND);
-        }
-
-        logger.info(`User updated. ID: ${id}`);
-        res.writeHead(STATUS_CODES.OK);
-        return res.end(JSON.stringify(result.data));
+      if (!result.data) {
+        logger.error(ERROR_MESSAGES.USER_NOT_FOUND, `User ID: ${id}`);
+        return sendError(res, STATUS_CODES.NOT_FOUND, ERROR_MESSAGES.USER_NOT_FOUND);
       }
 
-      if (method === METHODS.DELETE) {
-        const result = await executeDbCommand({ type: 'DELETE', id });
-
-        if (!result.success) {
-          logger.error(ERROR_MESSAGES.INTERNAL_SERVER_ERROR, `Error: ${result.error}`);
-          return sendError(res, STATUS_CODES.INTERNAL_SERVER_ERROR, result.error);
-        }
-
-        if (result.data === null && result.success) {
-          logger.error(ERROR_MESSAGES.USER_NOT_FOUND, `User ID: ${id}`);
-          return sendError(res, STATUS_CODES.NOT_FOUND, ERROR_MESSAGES.USER_NOT_FOUND);
-        }
-
-        logger.request(method, url, STATUS_CODES.NO_CONTENT);
-        logger.info(`User deleted. ID: ${id}`);
-        res.writeHead(STATUS_CODES.NO_CONTENT);
-        return res.end();
-      }
+      logger.request(method, url, STATUS_CODES.OK);
+      logger.info(`User updated. ID: ${id}`);
+      res.writeHead(STATUS_CODES.OK);
+      return res.end(JSON.stringify(result.data));
     }
 
-    return sendError(res, STATUS_CODES.NOT_FOUND, ERROR_MESSAGES.NOT_FOUND);
-  });
+    if (method === METHODS.DELETE) {
+      const result = await executeDbCommand({ type: 'DELETE', id });
+
+      if (!result.success) {
+        logger.error(ERROR_MESSAGES.INTERNAL_SERVER_ERROR, `Error: ${result.error}`);
+        return sendError(res, STATUS_CODES.INTERNAL_SERVER_ERROR, result.error);
+      }
+
+      if (result.data === null && result.success) {
+        logger.error(ERROR_MESSAGES.USER_NOT_FOUND, `User ID: ${id}`);
+        return sendError(res, STATUS_CODES.NOT_FOUND, ERROR_MESSAGES.USER_NOT_FOUND);
+      }
+
+      logger.request(method, url, STATUS_CODES.NO_CONTENT);
+      logger.info(`User deleted. ID: ${id}`);
+      res.writeHead(STATUS_CODES.NO_CONTENT);
+      return res.end();
+    }
+  }
+
+  return sendError(res, STATUS_CODES.NOT_FOUND, ERROR_MESSAGES.NOT_FOUND);
 };
+
+export const createAppServer = () => createServer(addHandler);
